4aaeb3447717fc02736b932e95133feef7b8b1f6
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-11-25 18:35:34 +0900

    category.rbやアソシエーションやルーティングなど

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,13 @@ class ItemsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1 } 
 end
