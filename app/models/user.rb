d1f9aaa24be9affd6d135c1e74aed6b1ca63e3cc
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-11-25 15:16:35 +0900

    ユーザー管理機能修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true
 
-  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'は英字と数字の両方を含めて設定してください' }
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください' }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
