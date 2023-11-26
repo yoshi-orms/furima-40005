3d88e5a70b3bc037f52b0643692d6a582d71c4ba
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-11-26 18:43:29 +0900

    ActiveStorageの導入作業

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   #has_one :order
+  has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :condition_id, numericality: { other_than: 1 } 
