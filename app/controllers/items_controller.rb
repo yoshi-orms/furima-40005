4c8b24d042ceb227801b85256994a081c1d8d2ac
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-11-27 14:56:26 +0900

    ログアウト状態時に出品を押下するとログインへ飛ぶ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new]
+  
   def index
   end
 
@@ -8,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+
   end
 
 end
