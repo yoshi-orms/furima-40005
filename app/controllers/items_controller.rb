40c33c8351ba4403d3ac6786467212c4a42b1684
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-11-27 15:25:11 +0900

    コントローラーへクリエイトを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+
   def index
   end
 
@@ -9,7 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path  
+    else
+      render :new
+    end
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price)
+  end
 end
