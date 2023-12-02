9facfebbd66f72c6994f58f93fd6101c5ee55b95
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-02 18:01:42 +0900

    クレジットカードのトークンテストコードを記述

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     street                   {Faker::Address.street_name}
     building                 {Faker::Address.secondary_address}
     phone_number             {"09012345678"}
+    token                    {"tok_abcdefghijk00000000000000000"}
   end
 end
