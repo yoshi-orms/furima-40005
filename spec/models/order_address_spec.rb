1cff762ff19c2674c186032e405d6f17a51b4c4c
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-03 02:01:19 +0900

    テストコード修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -9,7 +9,12 @@ RSpec.describe OrderAddress, type: :model do
 
   describe '商品購入機能' do
     context '商品購入できるとき' do
-      it 'zip_codeとprefecture_idとcityとstreetとbuildingとphone_numberが存在すれば購入できる' do
+      it '全ての項目が入力されていれば購入できる' do
+        expect(@order_address).to be_valid
+      end
+
+      it ' 建物名は任意であること' do
+        @order_address.building = ''
         expect(@order_address).to be_valid
       end
     end
@@ -33,6 +38,12 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include "Prefecture is not a number"
       end
 
+      it ' 都道府県に「---」が選択されている場合は購入できない' do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Prefecture must be other than 1"
+      end
+
       it ' 市区町村が必須であること' do
         @order_address.city = ''
         @order_address.valid?
@@ -43,11 +54,6 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.street = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include "Street can't be blank"
-      end
-
-      it ' 建物名は任意であること' do
-        @order_address.building = ''
-        expect(@order_address).to be_valid
       end
 
       it ' 電話番号が必須であること' do
@@ -62,10 +68,40 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include "Phone number is invalid"
       end
 
+      it ' 電話番号が9桁以下では購入できない' do
+        @order_address.phone_number = '0901234'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Phone number is invalid"
+      end
+
+      it ' 電話番号が12桁以上では購入できない' do
+        @order_address.phone_number = '090123456789'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Phone number is invalid"
+      end
+
+      it ' 電話番号に半角数字以外が含まれている場合は購入できない' do
+        @order_address.phone_number = '0901234567八9'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Phone number is invalid"
+      end
+
       it 'tokenが空では登録できないこと' do
         @order_address.token = nil
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include "Token can't be blank"
+      end
+
+      it 'userが紐付いていなければ購入できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "User can't be blank"
+      end
+
+      it 'itemが紐付いていなければ購入できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Item can't be blank"
       end
     end
   end
