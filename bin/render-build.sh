587f9fc0a56a4b26265fd540baef5da4c8767476
yoshi-orms <cat.o.r.m.haruma@gmail.com>
2023-12-03 16:04:08 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,6 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+rails assets:precompile
+bin/importmap
 #DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
